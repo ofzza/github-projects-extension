88037855fa503b2fe69c0ba11835b1605bb8cd16
ofzza <ofzza@ofzza.com>
2017-12-06 18:11:38 +0100

    Added persistence to selected search mode

--- src/content/github/projects/search.js
+++ src/content/github/projects/search.js
@@ -43,7 +43,7 @@ export default function bootstrap () { initSearchUI(); }
     let searchBarEl = document.createElement('input');
     searchBarEl.className = 'ogp-search-bar-element form-control form-control subnav-search-input input-contrast';
     searchBarEl.type = 'text';
-    searchBarEl.placeholder = 'Filter (using GitHub Projects Plus syntax)';
+    searchBarEl.placeholder = 'Filter cards (GitHub Projects Plus plugin)';
     $(searchBarContainerEl).append(searchBarEl);
     $(searchBarContainerEl).append(searchBarEl, '<svg aria-hidden="true" class="octicon octicon-search subnav-search-icon" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M15.7 13.3l-3.81-3.83A5.93 5.93 0 0 0 13 6c0-3.31-2.69-6-6-6S1 2.69 1 6s2.69 6 6 6c1.3 0 2.48-.41 3.47-1.11l3.83 3.81c.19.2.45.3.7.3.25 0 .52-.09.7-.3a.996.996 0 0 0 0-1.41v.01zM7 10.7c-2.59 0-4.7-2.11-4.7-4.7 0-2.59 2.11-4.7 4.7-4.7 2.59 0 4.7 2.11 4.7 4.7 0 2.59-2.11 4.7-4.7 4.7z"></path></svg>');
 
@@ -245,29 +245,43 @@ export default function bootstrap () { initSearchUI(); }
       searchTypeToggleButton.innerHTML = `
         <svg aria-hidden="true" class="octicon octicon-search subnav-search-icon" height="12" version="1.1" viewBox="0 0 16 16" width="12"><path fill-rule="evenodd" d="M15.7 13.3l-3.81-3.83A5.93 5.93 0 0 0 13 6c0-3.31-2.69-6-6-6S1 2.69 1 6s2.69 6 6 6c1.3 0 2.48-.41 3.47-1.11l3.83 3.81c.19.2.45.3.7.3.25 0 .52-.09.7-.3a.996.996 0 0 0 0-1.41v.01zM7 10.7c-2.59 0-4.7-2.11-4.7-4.7 0-2.59 2.11-4.7 4.7-4.7 2.59 0 4.7 2.11 4.7 4.7 0 2.59-2.11 4.7-4.7 4.7z"></path></svg>
       `;
-      $(searchTypeToggleButton).attr('title', 'Filter using GitHUb Projects Plus syntax');
-      $(searchTypeToggleButton).click(() => {
-        // Check which filter visible
-        if (searchBarContainerEl.style.display == 'none') {
-          $(searchTypeToggleButton).addClass('active');
-          $(searchBarContainerEl).css('display', 'block');
-          nativeSearchEl.css('display', 'none');
-        } else {
-          $(searchTypeToggleButton).removeClass('active');
-          $(searchBarContainerEl).css('display', 'none');
-          nativeSearchEl.css('display', 'block');
-        }
-      });
+      $(searchTypeToggleButton).attr('title', 'Filter cards (GitHub Projects Plus plugin)');
+      $(searchTypeToggleButton).click(() => { toggleSearch(searchBarContainerEl, searchTypeToggleButton, nativeSearchEl); });
       searchContainerEl.prepend(searchTypeToggleButton);
-
-      // Hide custom search
-      $(searchBarContainerEl).css('display', 'none');
+      
+      // Switch to native
+      toggleSearch(searchBarContainerEl, searchTypeToggleButton, nativeSearchEl, true);
 
     }
 
     // Insert tooltips container
     searchContainerEl.prepend(searchTooltipEl);
 
+  }
+
+  /**
+   * Toggles between GitHub native and custom search
+   */
+  function toggleSearch (searchBarContainerEl, searchTypeToggleButton, nativeSearchEl, initialize) {
+    // Select what to toggle to
+    let stored = window.localStorage.getItem('searchTypeNative'),
+        isNative = (typeof stored !== 'undefined' ? (stored && stored !== 'false') : (nativeSearchEl.style.display == 'block'));
+    // Check which filter visible
+    if (initialize ? isNative : !isNative) {
+      // Show native
+      $(searchTypeToggleButton).removeClass('active');
+      $(searchBarContainerEl).css('display', 'none');
+      nativeSearchEl.css('display', 'block');
+    } else {
+      // Show custom
+      $(searchTypeToggleButton).addClass('active');
+      $(searchBarContainerEl).css('display', 'block');
+      nativeSearchEl.css('display', 'none');
+    }
+    // Store state to local storage
+    if (!initialize) {
+      window.localStorage.setItem('searchTypeNative', !isNative);
+    }
   }
 
   /**
