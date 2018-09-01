844a291fb911e618380ffad9fee714f1319d3638
ofzza <ofzza@ofzza.com>
2018-09-01 22:00:25 +0200

    Updated manifest description; Dropped JS bootstrap until needed

--- src/content/bootstrap.js
+++ src/content/bootstrap.js
@@ -3,29 +3,29 @@
 // ============================================================================================
 
 // On load, bootstrap plugin
-window.addEventListener('load', () => {
+// window.addEventListener('load', () => {
 
-  // Check if other version already loaded (in case both GitHub.con and Enterprise versions installed)
-  if (document.body.getAttribute('ofzza-github-projects-optimizer')) { 
-    console.log('> GitHub Projects Optimizer: Another version already loaded ...');
-    return; 
-  } else {
-    document.body.setAttribute('ofzza-github-projects-optimizer', 'true');
-  }
+//   // Check if other version already loaded (in case both GitHub.con and Enterprise versions installed)
+//   if (document.body.getAttribute('ofzza-github-projects-optimizer')) { 
+//     console.log('> GitHub Projects Optimizer: Another version already loaded ...');
+//     return; 
+//   } else {
+//     document.body.setAttribute('ofzza-github-projects-optimizer', 'true');
+//   }
 
-  // Check if github page
-  let metaEls = document.getElementsByTagName('meta'),
-      foundGitHubMetaElement = false;
-  for (let i in metaEls) {
-    if ((metaEls[i]['name'] === 'octolytics-app-id') && (metaEls[i]['content'] === 'github')) {
-      foundGitHubMetaElement = true;
-      break;
-    }
-  }
-  if (foundGitHubMetaElement || window.location.hostname.match(/github/)) {
+//   // Check if github page
+//   let metaEls = document.getElementsByTagName('meta'),
+//       foundGitHubMetaElement = false;
+//   for (let i in metaEls) {
+//     if ((metaEls[i]['name'] === 'octolytics-app-id') && (metaEls[i]['content'] === 'github')) {
+//       foundGitHubMetaElement = true;
+//       break;
+//     }
+//   }
+//   if (foundGitHubMetaElement || window.location.hostname.match(/github/)) {
     
-    // TODO: ...
+//     // TODO: ...
 
-  }
+//   }
   
-});
+// });
