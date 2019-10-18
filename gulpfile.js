388052cba55a21d4dfb4c4c7c7099b756d6a5829
ofzza <ofzza@ofzza.com>
2019-10-18 13:39:28 +0200

    Updated build (gulp4, etc.) to make it compatible with node 12+

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,6 @@ const _           = require('lodash'),
       fs          = require('fs'),
       gulp        = require('gulp'),
       util        = require('gulp-util');
-      gulpsync    = require('gulp-sync')(gulp);
       clean       = require('gulp-clean'),
       less        = require('gulp-less'),
       babel       = require('gulp-babel'),
@@ -26,8 +25,7 @@ const _           = require('lodash'),
       buffer      = require('vinyl-buffer'),
       sourcemaps  = require('gulp-sourcemaps'),
       uglify      = require('gulp-uglify'),
-      zip         = require('gulp-zip'),
-      watch       = require('gulp-watch');
+      zip         = require('gulp-zip');
 
 // Load package info
 // --------------------------------------------------------------------------------------------------------------------
@@ -49,10 +47,11 @@ gulp.task('mainfest@build', () => {
       enterpriseManifest = _.merge({}, manifest, require('./src/manifest.enterprise.json'), { version: pck.version });
   fs.writeFileSync('./dist/github.com/manifest.json', JSON.stringify(githubManifest));
   fs.writeFileSync('./dist/enterprise/manifest.json', JSON.stringify(enterpriseManifest));
+  return Promise.resolve();
 });
 // ... and attached watcher
 gulp.task('watch.mainfest@build', () => {
-  watch(['./src/**/*.json'], () => { gulp.start('mainfest@build'); });
+  gulp.watch(['./src/**/*.json'], gulp.series('mainfest@build'));
 });
 
 // Define copy task
@@ -68,7 +67,7 @@ gulp.task('copy@build', () => {
 });
 // ... and attached watcher
 gulp.task('watch.copy@build', () => {
-  watch(otherFiletypes, () => { gulp.start('copy@build'); });
+  gulp.watch(otherFiletypes, gulp.series('copy@build'));
 });
 
 // ... and library copy task
@@ -89,7 +88,7 @@ gulp.task('less@build', () => {
 });
 // ... and attached watcher
 gulp.task('watch.less@build', () => {
-  watch('./src/content/github/**/*.less', () => { gulp.start('less@build'); });
+  gulp.watch('./src/content/github/**/*.less', gulp.series('less@build'));
 });
 
 // Define ES6 transpile task
@@ -99,7 +98,8 @@ gulp.task('transpile@build', () => {
     entries: ['./src/content/bootstrap.js'],
     debug: !util.env.production
   })
-    .transform(['babelify', {presets: ['es2015']}]).bundle()
+    .transform(['babelify', {presets: ['@babel/preset-env']}])
+    .bundle()
     .pipe(source('bootstrap.js'))
     .pipe(buffer())
     .pipe(!util.env.production ? sourcemaps.init() : util.noop())
@@ -110,7 +110,7 @@ gulp.task('transpile@build', () => {
 });
 // ... and attached watcher
 gulp.task('watch.transpile@build', () => {
-  watch('./src/**/*.js', () => { gulp.start('transpile@build'); });
+  gulp.watch('./src/**/*.js', gulp.series('transpile@build'));
 });
 
 // Define zip task
@@ -125,17 +125,14 @@ gulp.task('zip.enterprise@build', () => {
     .pipe(zip(`dist.enterprise.v${pck.version}.zip`))
     .pipe(gulp.dest(`./versions/${util.env.production ? 'prod' : 'dev'}`));
 });
-gulp.task('zip.build', ['zip.githubcom@build', 'zip.enterprise@build' ]);
+gulp.task('zip.build', gulp.series(['zip.githubcom@build', 'zip.enterprise@build' ]));
 // ... and attached watcher
 gulp.task('watch.zip@build', () => {
-  watch(['./dist/**/*'], () => {
-    gulp.start('zip.githubcom@build'); 
-    gulp.start('zip.enterprise@build'); 
-  });
+  gulp.watch(['./dist/**/*'], gulp.series(['zip.githubcom@build', 'zip.enterprise@build']));
 });
 
 
 // Define root tasks
 // --------------------------------------------------------------------------------------------------------------------
-gulp.task('build', gulpsync.sync(['clear@build', 'copy@build', 'less@build', 'copy@libs', 'transpile@build', 'mainfest@build', 'zip.build']));
-gulp.task('watch', ['watch.copy@build', 'watch.less@build', 'watch.transpile@build', 'watch.mainfest@build', 'watch.zip@build']);
+gulp.task('build', gulp.series(['clear@build', 'copy@build', 'less@build', /*'copy@libs',*/ 'transpile@build', 'mainfest@build', 'zip.build']));
+gulp.task('watch', gulp.parallel(['watch.copy@build', 'watch.less@build', 'watch.transpile@build', 'watch.mainfest@build', 'watch.zip@build']));
